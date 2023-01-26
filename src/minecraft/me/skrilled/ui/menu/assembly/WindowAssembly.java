e7c32c28d8625bac6118f0b90f3999bccf1b6925
Prohect <Prohect@foxmail.com>
2023-01-26 08:33:16 +0800

    GUI架构模块化

--- src/minecraft/me/skrilled/ui/menu/assembly/WindowAssembly.java
+++ src/minecraft/me/skrilled/ui/menu/assembly/WindowAssembly.java
@@ -6,23 +6,30 @@ public class WindowAssembly extends Assembly{
     BGAssembly bgAssembly;
     StringAssembly windowName;
     ArrayList<IconAssembly> icons;
-    ArrayList<WindowAssembly> windowAssemblies;
+    ArrayList<WindowAssembly> subWindows;
     ArrayList<Assembly> otherAssemblies;
     public WindowAssembly(float[] pos, Assembly fatherWindow) {
         super(pos, fatherWindow);
     }
 
+    public WindowAssembly(float[] pos, Assembly fatherWindow,ArrayList<WindowAssembly> subWindows) {
+        super(pos, fatherWindow);
+        this.subWindows = subWindows;
+    }
+
     @Override
     public void draw() {
         this.bgAssembly.draw();
+        if (windowName!=null){
         this.windowName.draw();
+        }
         for (IconAssembly icon : this.icons) {
             icon.draw();
         }
         for (Assembly otherAssembly : otherAssemblies) {
             otherAssembly.draw();
         }
-        for (WindowAssembly windowAssembly : windowAssemblies) {
+        for (WindowAssembly windowAssembly : subWindows) {
             windowAssembly.draw();
         }
     }
