00a0b4ab60fc3356eab507fbfbdca7625b306626
Prohect <Prohect@foxmail.com>
2023-01-28 18:39:41 +0800

    GUI架构模块化

--- src/minecraft/me/skrilled/ui/menu/assembly/IconAssembly.java
+++ src/minecraft/me/skrilled/ui/menu/assembly/IconAssembly.java
@@ -1,12 +1,40 @@
 package me.skrilled.ui.menu.assembly;
 
+import me.fontloader.FontDrawer;
+import me.skrilled.utils.render.RenderUtil;
+import me.surge.animation.Animation;
+
+import java.awt.*;
+
 public class IconAssembly extends Assembly {
-    public IconAssembly(float[] pos, Assembly fatherWindow) {
+    FontDrawer font;
+    String[] icons;
+    float spacing;
+    Animation anim;
+    Color iconColor;
+    Color bgColor;
+    Color currentColor;
+    boolean isTransverse;
+
+    private IconAssembly(float[] pos, Assembly fatherWindow) {
         super(pos, fatherWindow);
+    }
+
+    public IconAssembly(float[] pos, Assembly fatherWindow, FontDrawer font, String[] icons, float spacing, Animation anim, Color iconColor, Color bgColor, Color currentColor, boolean isTransverse) {
+        super(pos, fatherWindow);
+        this.font = font;
+        this.icons = icons;
+        this.spacing = spacing;
+        this.anim = anim;
+        this.iconColor = iconColor;
+        this.bgColor = bgColor;
+        this.currentColor = currentColor;
+        this.isTransverse = isTransverse;
     }
 
     @Override
     public void draw() {
-
+        float absX=calcAbsX(),absY = calcAbsY();
+        RenderUtil.drawTitleIcon(font,absX,absY,icons,spacing,(float) anim.getAnimationFactor(),iconColor.getRGB(),bgColor.getRGB(),currentColor.getRGB(),isTransverse);
     }
 }
