5aedb5b57e5bd913fd3919f52c4f1c055bd7cd99
Skrilled0v0 <Skrilled___@outlook.com>
2023-02-01 14:40:58 +0800

    Fixed

--- src/minecraft/net/minecraft/client/gui/GuiButton.java
+++ src/minecraft/net/minecraft/client/gui/GuiButton.java
@@ -2,6 +2,8 @@ package net.minecraft.client.gui;
 
 
 import me.fontloader.FontDrawer;
+import me.skrilled.SenseHeader;
+import me.skrilled.ui.Notification;
 import me.skrilled.utils.IMC;
 import me.skrilled.utils.render.BlurUtil;
 import me.skrilled.utils.render.RenderUtil;
@@ -18,6 +20,11 @@ import java.awt.*;
 
 public class GuiButton extends Gui implements IMC {
     protected static final ResourceLocation buttonTextures = new ResourceLocation("textures/gui/widgets.png");
+    private final Animation motion = new Animation(1000f, false, Easing.LINEAR);
+    //    ColourAnimation upBgMotion = new ColourAnimation(new Color(255, 255, 255, 50), new Color(108, 108, 255, 75), 1000f, false, Easing.LINEAR);
+//    ColourAnimation downBgMotion = new ColourAnimation(new Color(108, 108, 255, 50), new Color(255, 255, 255, 75), 1000f, false, Easing.LINEAR);
+    private final ColourAnimation rectMotion = new ColourAnimation(new Color(10, 10, 10, 35), new Color(10, 10, 10, 100), 1000f, false, Easing.LINEAR);
+    private final ColourAnimation strMotion = new ColourAnimation(new Color(255, 255, 255), new Color(255, 140, 0), 1000f, false, Easing.LINEAR);
     public int xPosition;
     public int yPosition;
     public String displayString;
@@ -26,12 +33,9 @@ public class GuiButton extends Gui implements IMC {
     public boolean visible;
     protected int width;
     protected int height;
-    protected boolean hovered;
-    Animation motion = new Animation(1000f, false, Easing.LINEAR);
-    //    ColourAnimation upBgMotion = new ColourAnimation(new Color(255, 255, 255, 50), new Color(108, 108, 255, 75), 1000f, false, Easing.LINEAR);
-//    ColourAnimation downBgMotion = new ColourAnimation(new Color(108, 108, 255, 50), new Color(255, 255, 255, 75), 1000f, false, Easing.LINEAR);
-    ColourAnimation rectMotion = new ColourAnimation(new Color(10, 10, 10, 35), new Color(10, 10, 10, 100), 1000f, false, Easing.LINEAR);
-    ColourAnimation strMotion = new ColourAnimation(new Color(255, 255, 255), new Color(255, 140, 0), 1000f, false, Easing.LINEAR);
+    public boolean hovered;
+    public Animation widthMotion;
+    public Animation heightMotion;
 
     public GuiButton(int buttonId, int x, int y, String buttonText) {
         this(buttonId, x, y, 200, 20, buttonText);
@@ -48,23 +52,21 @@ public class GuiButton extends Gui implements IMC {
         this.displayString = buttonText;
     }
 
-    public int getxPosition() {
-        return xPosition;
-    }
-
-    public void setxPosition(int xPosition) {
+    public GuiButton(int id,int xPosition, int yPosition,  int width, int height,String displayString,  Animation widthMotion, Animation heightMotion) {
+        this.enabled = true;
+        this.visible = true;
         this.xPosition = xPosition;
-    }
-
-    public int getyPosition() {
-        return yPosition;
-    }
-
-    public void setyPosition(int yPosition) {
         this.yPosition = yPosition;
+        this.displayString = displayString;
+        this.id = id;
+        this.width = width;
+        this.height = height;
+        this.widthMotion = widthMotion;
+        this.heightMotion = heightMotion;
     }
 
     public void drawButton(Minecraft mc, int mouseX, int mouseY) {
+        System.out.println(hovered);
 /*
              四周border逐渐补全绘制方法
             //up line
@@ -76,23 +78,25 @@ public class GuiButton extends Gui implements IMC {
             //left line
             RenderUtil.drawRect(this.xPosition, this.yPosition + height - height * motion.getAnimationFactor(), this.xPosition + lineWidth, this.yPosition + height, rectMotion.getColour().getRGB());
 */
+        FontDrawer font = Main.fontLoader.EN18;
+        float xMotion = 0f;
+        float yMotion = 0f;
+        if (widthMotion != null) {
+            xMotion =(1-(float) widthMotion.getAnimationFactor())* -(float) (this.xPosition + width + 4f * motion.getAnimationFactor());
+        }
+        if (heightMotion != null) {
+            yMotion = (1 - (float) heightMotion.getAnimationFactor())*-(float) (this.yPosition + height + 2f * motion.getAnimationFactor());
+        }
+
         if (this.visible) {
+
             startMotion(hovered);
-            FontDrawer font = Main.fontLoader.EN18;
             this.hovered = mouseX >= this.xPosition && mouseY >= this.yPosition && mouseX < this.xPosition + this.width && mouseY < this.yPosition + this.height;
-            RenderUtil.drawRoundRect(
-                    (float) (this.xPosition - 4f * motion.getAnimationFactor()),
-                    (float) (this.yPosition - 2f * motion.getAnimationFactor()),
-                    (float) (this.xPosition + width + 4f * motion.getAnimationFactor()),
-                    (float) (this.yPosition + height + 2f * motion.getAnimationFactor()), 5, rectMotion.getColour().getRGB());
-            BlurUtil.blurAreaRounded(
-                    (float) (this.xPosition - 4f * motion.getAnimationFactor()),
-                    (float) (this.yPosition - 2f * motion.getAnimationFactor()),
-                    (float) (this.xPosition + width + 4f * motion.getAnimationFactor()),
-                    (float) (this.yPosition + height + 2f * motion.getAnimationFactor()), 5, 10);
+            RenderUtil.drawRoundRect((float) (this.xPosition - 4f * motion.getAnimationFactor()) + xMotion, (float) (this.yPosition - 2f * motion.getAnimationFactor()) + yMotion, (float) (this.xPosition + width + 4f * motion.getAnimationFactor()) + xMotion, (float) (this.yPosition + height + 2f * motion.getAnimationFactor()) + yMotion, 5, rectMotion.getColour().getRGB());
+            BlurUtil.blurAreaRounded((float) (this.xPosition - 4f * motion.getAnimationFactor()) + xMotion, (float) (this.yPosition - 2f * motion.getAnimationFactor()) + yMotion, (float) (this.xPosition + width + 4f * motion.getAnimationFactor()) + xMotion, (float) (this.yPosition + height + 2f * motion.getAnimationFactor()) + yMotion, 5, 10);
 
             this.mouseDragged(mc, mouseX, mouseY);
-            font.drawCenteredString(this.displayString, this.xPosition + this.width / 2f, this.yPosition + (this.height - 8) / 2f, strMotion.getColour().getRGB());
+            font.drawCenteredString(this.displayString, this.xPosition + this.width / 2f + xMotion, this.yPosition + yMotion + (this.height - 8) / 2f, strMotion.getColour().getRGB());
         }
     }
 
--- src/minecraft/net/minecraft/client/gui/GuiMainMenu.java
+++ src/minecraft/net/minecraft/client/gui/GuiMainMenu.java
@@ -1,77 +1,67 @@
 package net.minecraft.client.gui;
 
 
-import me.skrilled.utils.IMC;
-import me.skrilled.utils.render.BlurUtil;
+import me.fontloader.FontDrawer;
+import me.skrilled.SenseHeader;
+import me.skrilled.ui.alt.GuiAltLogin;
 import me.skrilled.utils.render.RenderUtil;
 import me.surge.animation.Animation;
 import me.surge.animation.ColourAnimation;
 import me.surge.animation.Easing;
 import net.minecraft.client.main.Main;
 import net.minecraft.client.renderer.GlStateManager;
+import net.minecraft.client.resources.I18n;
 
 import java.awt.*;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Collections;
 
-public class GuiMainMenu extends GuiScreen implements GuiYesNoCallback, IMC {
+public class GuiMainMenu extends GuiScreen implements GuiYesNoCallback {
 
-    Animation sideBarMotion = new Animation(1200f, false, Easing.QUAD_OUT);
-    Animation singleplayer = new Animation(1200f, false, Easing.QUAD_OUT);
-    Animation multiplayer = new Animation(1200f, false, Easing.QUAD_OUT);
-    Animation alt = new Animation(1200f, false, Easing.QUAD_OUT);
-    Animation settings = new Animation(1200f, false, Easing.QUAD_OUT);
-    Animation exit = new Animation(1200f, false, Easing.QUAD_OUT);
-    ArrayList<Animation> aniList = new ArrayList<>();
+    Animation animation = new Animation(1500f, false, Easing.CUBIC_OUT);
 
-    ColourAnimation sideBarColor = new ColourAnimation(new Color(0, 0, 0, 30), new Color(0, 0, 0, 80), 1500f, false, Easing.LINEAR);
+    ColourAnimation colourAnimation = new ColourAnimation(new Color(0, 0, 0, 30), new Color(0, 0, 0, 100), 1500f, false, Easing.LINEAR);
 
-    @Override
     public void initGui() {
-        Collections.addAll(aniList, singleplayer, multiplayer, alt, settings, exit);
-        super.initGui();
+        int interval = 30;
+        int buttonWidth = 100;
+        int buttonHeight = 20;
+        int j = this.height / 2 - buttonHeight / 2;
+        this.buttonList.add(new GuiButton(1, interval, j + interval * 2, buttonWidth, buttonHeight, I18n.format("menu.quit"), animation, animation));
+        this.buttonList.add(new GuiButton(2, interval, j + interval, buttonWidth, buttonHeight, I18n.format("menu.options"), animation, animation));
+        this.buttonList.add(new GuiButton(3, interval, j, buttonWidth, buttonHeight, "Alt:" + mc.session.getUsername(), animation, animation));
+        this.buttonList.add(new GuiButton(4, interval, j - interval, buttonWidth, buttonHeight, I18n.format("menu.multiplayer"), animation, animation));
+        this.buttonList.add(new GuiButton(5, interval, j - interval * 2, buttonWidth, buttonHeight, I18n.format("menu.singleplayer"), animation, animation));
+    }
+
+
+    protected void actionPerformed(GuiButton button) throws IOException {
+
+        if (button.id == 5) mc.displayGuiScreen(new GuiSelectWorld(this));
+
+        if (button.id == 4) mc.displayGuiScreen(new GuiMultiplayer(this));
+
+        if (button.id == 3) mc.displayGuiScreen(new GuiAltLogin(this));
+
+        if (button.id == 2) mc.displayGuiScreen(new GuiOptions(this, mc.gameSettings));
+
+        if (button.id == 1) SenseHeader.getSense.stopClient();
+
     }
 
     public void drawScreen(int mouseX, int mouseY, float partialTicks) {
+        animation.setState(isHovering(mouseX, mouseY, 0, 0, width / 4f, height));
+        colourAnimation.setState(isHovering(mouseX, mouseY, 0, 0, width / 4f, height));
         this.drawBackground();
+        int j = this.height / 2;
+        FontDrawer font = Main.fontLoader.EN48;
+
         GlStateManager.pushMatrix();
+        RenderUtil.drawRect(0, 0, width / 4f * animation.getAnimationFactor(), height*animation.getAnimationFactor(), colourAnimation.getColour().getRGB());
+        font.drawRainbowString(SenseHeader.getSense.getClientName(), 30, j - 90);
 
-        RenderUtil.drawRect(0, 0, width * 0.16f * sideBarMotion.getAnimationFactor(), height, sideBarColor.getColour().getRGB());
-
-        float onceHeight = height*0.8f/5f;
-        float lMargin = width * 0.035f;
-        float spacing = height * 0.065f;
-        float boxWidth = width * 0.2f;
-        for (int j = 0; j < aniList.size(); j++) {
-            BlurUtil.blurAreaRounded((float) (lMargin * aniList.get(j).getAnimationFactor()), height / 2f - (2.5f - j) * onceHeight - (2 - j) * spacing, (float) (lMargin + boxWidth * aniList.get(j).getAnimationFactor()), height / 2f - (2.5f - j) * onceHeight - (2 - j) * spacing + onceHeight, Main.fontLoader.EN16.getHeight() / 2f, 10);
-            RenderUtil.drawCenteredStringBoxWith4PosWithOutAutoNextLine(new float[]{},
-                    Main.fontLoader.EN16, "aniList.get(j).toString()", new Color(0, 0, 0, 35).getRGB(), -1);
-            if (isHovering(mouseX, mouseY, lMargin, height / 2f - (2.5f - j) * onceHeight - (2 - j) * spacing, lMargin + boxWidth, height / 2f - (2.5f - j) * onceHeight - (2 - j) * spacing + onceHeight)) {
-                System.out.println(aniList.get(j) + "   | T");
-                aniList.get(j).setState(true);
-
-            } else {
-//                System.out.println(aniList.get(j)+"   | F");
-                aniList.get(j).setState(false);
-            }
-
-        }
         GlStateManager.popMatrix();
-        setMotion(isHovering(mouseX, mouseY, 0, 0, width / 6f, height));
+
         super.drawScreen(mouseX, mouseY, partialTicks);
-    }
-
-    private void setMotion(boolean flag) {
-        sideBarColor.setState(flag);
-        sideBarMotion.setState(flag);
-    }
-
-    private void setListMotion(int index, boolean flag) {
-        for (int z = 0; z < aniList.size(); z++) {
-            if (z == index) aniList.get(z).setState(flag);
-            else aniList.get(z).setState(!flag);
-        }
     }
 
     private boolean isHovering(int mouseX, int mouseY, float xLeft, float yUp, float xRight, float yBottom) {
