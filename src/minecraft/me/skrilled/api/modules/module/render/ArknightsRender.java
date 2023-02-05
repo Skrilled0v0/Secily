bbc961c619bfc7a27cbd4e8af8b57440102864b2
Skrilled0v0 <Skrilled___@outlook.com>
2023-02-05 18:19:48 +0800

    Gui Fix

--- src/minecraft/me/skrilled/api/modules/module/render/ArknightsRender.java
+++ src/minecraft/me/skrilled/api/modules/module/render/ArknightsRender.java
@@ -5,6 +5,6 @@
  */
 package me.skrilled.api.modules.module.render;
 
-public class AknightsRender {
+public class ArknightsRender {
     // TODO: 2023/1/27  明日方舟干员绘制Module
 }
--- src/minecraft/me/skrilled/utils/render/arknightsrenderer/ArknightsCapableOffcials.java
+++ src/minecraft/me/skrilled/utils/render/arknightsrenderer/ArknightsCapableOffcials.java
@@ -3,7 +3,7 @@
  *Code by SkrilledSense
  *20230201
  */
-package me.skrilled.utils.render.aknightsrenderer;
+package me.skrilled.utils.render.arknightsrenderer;
 
 import net.minecraft.util.ResourceLocation;
 
@@ -11,12 +11,12 @@ import java.util.ArrayList;
 
 // TODO: 2023/2/1 明日方舟干员模型构造器
 //  动作有 Move（移动）Interact（点击）Relax（闲置）Special（特殊、随机触发）每种对应一套动作的 ResourceLocation List
-public class AknightsCapableOffcials {
+public class ArknightsCapableOffcials {
     ArrayList<ResourceLocation> resourceLocations;
     int maxIndex;
     String name;
 
-    public AknightsCapableOffcials(String name,ArrayList<ResourceLocation> resourceLocations, int maxIndex) {
+    public ArknightsCapableOffcials(String name, ArrayList<ResourceLocation> resourceLocations, int maxIndex) {
         this.resourceLocations = resourceLocations;
         this.maxIndex = maxIndex;
         this.name = name;
